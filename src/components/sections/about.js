9387ca80b62b48b5f35440f743ef809a28cdf710
Naitik <shahnaitik2000@gmail.com>
2022-06-05 18:09:55 +0530

    Error in about.js

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -136,8 +136,6 @@ const About = () => {
         <StyledText>
           <div>
             <p>
-          <div>
-            <p>
               Hello! My name is Brittany and I enjoy creating things that live on the internet. My
               interest in web development started back in 2012 when I decided to try editing custom
               Tumblr themes — turns out hacking together a custom reblog button taught me a lot
