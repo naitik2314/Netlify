2d389c7922855caecd60b09a5137ea13900bd3a0
Naitik <shahnaitik2000@gmail.com>
2022-09-20 11:24:53 +0530

    Added skills

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -126,7 +126,7 @@ const About = () => {
     sr.reveal(revealContainer.current, srConfig());
   }, []);
 
-  const skills = ['JavaScript (ES6+)', 'TypeScript', 'React', 'Python', 'Node.js', 'Flutter', 'DevOps', 'AI'];
+  const skills = ['JavaScript (ES6+)', 'TypeScript', 'React', 'Python', 'Node.js', 'Flutter', 'DevOps', 'AI', 'MLOps', 'Hadoop'];
 
   return (
     <StyledAboutSection id="about" ref={revealContainer}>
